9fa63e479222911e150edf9e773ccacbdff98706
Santiago Quiroga <quirogas@mail.gvsu.edu>
2019-11-18 11:23:18 -0500

    added some base checks

--- src/rocket/rocket_data_tracker.py
+++ src/rocket/rocket_data_tracker.py
@@ -1,15 +1,17 @@
 import krpc
 import numpy as np
 import numpy.linalg as la
-
+import time 
+from typing import List
 class RocketData:
 
     def __init__(self, connection):
         DEST_LOC = np.array((-0.49409718944856473, 59.473877823389955))
         self.connection = connection
         self.mun_target = connection.space_center.bodies['Mun']
-        self.connection.space_center.target_body = self.mun_target
-        vessel = connection.space_center.active_vessel
+        self.connection.space_center.target_body = self.mun_target #what is this line of code doing ?
+        self.vessel = connection.space_center.active_vessel
+        self.start_time = time.time() # I get the initial time of the program in order to keep track of duration
 
 
         self.position = connection.add_stream(vessel.position, self.mun_target.reference_frame)
@@ -23,6 +25,7 @@ class RocketData:
         return la.norm(basis_dist)
 
     def get_situation(self):
+        # Isn't this situation the one retrieved from the vesel ?
         return self.situation()
 
     def get_orbit_data(self):
@@ -38,16 +41,29 @@ class RocketData:
         orbit = self.orbit()
         return np.array((orbit.body, orbit.apoapsis_altitude, orbit.periapsis_altitude))
 
+    def getResourcesAmount(self, vessel) -> List[float]:
+        return [self.vessel.resourses.amount["LiquidFuel"], self.vessel.resourses.amount["Oxidizer"], self.vessel.resourses.amount["ElectricCharge"]]
+
+    def getSituation(self) -> krpc.VesselSituatuion:
+        return self.vessel.situation
+
+
     @property
     def asses_fligh(self) -> bool:
-      # zero fuel condition
       # zero altitude after x time condition
+      if (self.vessel.met > 10 and vessel.situation == VesselSituatuion.pre_launch):
+          return False
 
-      # wrong direction condition sphere of influence closeset approach value  should be betweeen 150 and  
-      # checking if our sphere of inlfuence is not the moon or the earth 
-
-      # damaged rocket condition
-      # Insuficient electric power
       # vesel not in ocean condition 
-      # vesel not escaping condition 
-      # rocket got detroyed condition
+      if (self.vessel.situation == krpc.VesselSituatuion.docked 
+              and self.vessel.situation == krpc.VesselSituatuion.landed 
+              and self.vessel.situation == krpc.VesselSituatuion.splashed):
+          return False  
+
+      # zero fuel condition
+      # Insuficient electric power
+      if (self.vessel.resourses.amount["LiquidFuel"] == 0 
+              or self.vessel.resourses.amount["Oxidizer"] == 0):
+          return False
+
+      # zero electric charge condition
