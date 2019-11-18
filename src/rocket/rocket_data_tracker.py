db54f2868913177ab5f6643fb10c3cd22064fc18
David Baas <Dcbaas@users.noreply.github.com>
2019-11-18 17:29:39 -0500

    fixed merge conflict in rocket_data_tracker.py

--- src/rocket/rocket_data_tracker.py
+++ src/rocket/rocket_data_tracker.py
@@ -1,30 +1,32 @@
 import krpc
 import numpy as np
 import numpy.linalg as la
-import time 
+import time
 from typing import List
+
+
 class RocketData:
 
     def __init__(self, connection):
         DEST_LOC = np.array((-0.49409718944856473, 59.473877823389955))
         self.connection = connection
         self.mun_target = connection.space_center.bodies['Mun']
-<<<<<<< HEAD
-        self.connection.space_center.target_body = self.mun_target #what is this line of code doing ?
+
+        self.connection.space_center.target_body = self.mun_target  # what is this line of code doing ?
         self.vessel = connection.space_center.active_vessel
-        self.start_time = time.time() # I get the initial time of the program in order to keep track of duration
-=======
+        self.start_time = time.time()  # I get the initial time of the program in order to keep track of duration
+
         self.connection.space_center.target_body = self.mun_target
-        vessel = connection.space_center.active_vessel
-        flight = vessel.flight(vessel.orbit.body.reference_frame)
->>>>>>> 6b052627d498ba9fb7f877ff57f69c88f688d1ea
+        # vessel = connection.space_center.active_vessel
+        flight = self.vessel.flight(self.vessel.orbit.body.reference_frame)
 
         self.speed = connection.add_stream(getattr, flight, 'speed')
 
-        self.position = connection.add_stream(vessel.position, self.mun_target.reference_frame)
-        self.CARTESIAN_POS = np.array(self.mun_target.surface_position(DEST_LOC[0], DEST_LOC[1], self.mun_target.reference_frame))
-        self.situation = connection.add_stream(getattr, vessel, 'situation')
-        self.orbit = connection.add_stream( getattr, vessel, 'orbit')
+        self.position = connection.add_stream(self.vessel.position, self.mun_target.reference_frame)
+        self.CARTESIAN_POS = np.array(
+            self.mun_target.surface_position(DEST_LOC[0], DEST_LOC[1], self.mun_target.reference_frame))
+        self.situation = connection.add_stream(getattr, self.vessel, 'situation')
+        self.orbit = connection.add_stream(getattr, self.vessel, 'orbit')
 
     def get_distance(self):
         basis_dist = self.CARTESIAN_POS - np.array(self.position())
@@ -48,38 +50,33 @@ class RocketData:
         :return: an np array with the fields above
         """
         orbit = self.orbit()
-<<<<<<< HEAD
+
         return np.array((orbit.body, orbit.apoapsis_altitude, orbit.periapsis_altitude))
 
     def getResourcesAmount(self, vessel) -> List[float]:
-        return [self.vessel.resourses.amount["LiquidFuel"], self.vessel.resourses.amount["Oxidizer"], self.vessel.resourses.amount["ElectricCharge"]]
+        return [self.vessel.resourses.amount["LiquidFuel"], self.vessel.resourses.amount["Oxidizer"],
+                self.vessel.resourses.amount["ElectricCharge"]]
 
     def getSituation(self) -> krpc.VesselSituatuion:
         return self.vessel.situation
 
-
     @property
     def asses_fligh(self) -> bool:
-      # zero altitude after x time condition
-      if (self.vessel.met > 10 and vessel.situation == VesselSituatuion.pre_launch):
-          return False
+        # zero altitude after x time condition
+        if self.vessel.met > 10 and self.vessel.situation == self.vessel.situation.pre_launch:
+            return False
 
-      # vesel not in ocean condition 
-      if (self.vessel.situation == krpc.VesselSituatuion.docked 
-              and self.vessel.situation == krpc.VesselSituatuion.landed 
-              and self.vessel.situation == krpc.VesselSituatuion.splashed):
-          return False  
+        # vesel not in ocean condition
+        if (self.vessel.situation == krpc.VesselSituatuion.docked
+                and self.vessel.situation == krpc.VesselSituatuion.landed
+                and self.vessel.situation == krpc.VesselSituatuion.splashed):
+            return False
 
-      # zero fuel condition
-      # Insuficient electric power
-      if (self.vessel.resourses.amount["LiquidFuel"] == 0 
-              or self.vessel.resourses.amount["Oxidizer"] == 0):
-          return False
-
-      # zero electric charge condition
-=======
-        return orbit.body.name, orbit.apoapsis_altitude, orbit.periapsis_altitude
+            # zero fuel condition
+        # Insuficient electric power
+        if (self.vessel.resourses.amount["LiquidFuel"] == 0
+                or self.vessel.resourses.amount["Oxidizer"] == 0):
+            return False
 
     def get_closest_approach(self):
         return self.mun_target.orbit.distance_at_closest_approach(self.orbit())
->>>>>>> 6b052627d498ba9fb7f877ff57f69c88f688d1ea
