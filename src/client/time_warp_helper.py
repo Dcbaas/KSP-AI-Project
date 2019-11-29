3265b4ecf4134a13ee0e6fa935a143a6d7910b15
David Baas <Dcbaas@users.noreply.github.com>
2019-11-29 14:19:22 -0500

    added comments for the warp helper

--- src/client/time_warp_helper.py
+++ src/client/time_warp_helper.py
@@ -4,11 +4,24 @@ https://www.youtube.com/watch?v=ZanHo6Wn1H4
 """
 
 class TimeWarpHelper:
+    """
+    Our original goal was to get a rocket to the Mun. In order to do that we needed to manipulate the warp tool that
+    is provided by KSP. We would fly towards the Mun and then warp closer and then contiue with our simulation
+    """
     WARP_FACTOR_LIMIT = 5
     def __init__(self, connection):
+        """
+        initialize the warp helper. The warp helper need a connection to KSP.
+        :param connection: The connection to KSP
+        """
         self.connection = connection
 
     def warp_till_body_shift(self):
+        """
+        Warps the rocket towards the Mun. The trigger for exiting the warp helper was a when the rocket left the
+        sphere of influence of Kerbin.
+        :return: void 
+        """
         space_center = self.connection.space_center
         vessel_orbit = space_center.active_vessel.orbit
         if not space_center.can_rails_warp_at(1):
